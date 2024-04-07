1efac3ce1d602c97569419b918c006257880f6ef
komob <b.komorniczak@gmail.com>
2024-04-07 12:59:13 +0200

    surprisingly working

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,22 @@
 use clap::{App, Arg};
 use std::{fs, io};
 
+fn caesar_cipher(input: &str, key: i32) -> String {
+    input.to_uppercase()
+        .chars()
+        .filter(|c| c.is_alphabetic())
+        .map(|c| {
+            let first_letter = b'A';
+            let shifted = ((c as u8 - first_letter + (key.rem_euclid(26) as u8))).rem_euclid(26) + first_letter;
+            shifted as char
+        })
+        .collect()
+}
 
 fn main() -> io::Result<()> {
     let matches = App::new("Cezar")
         .version("1.0")
-        .author("Komob")
-        .about("Lab1 KK")
+        .about("Lab2 KK")
         .arg(
             Arg::with_name("input")
                 .short('i')
@@ -22,13 +32,25 @@ fn main() -> io::Result<()> {
                 .takes_value(true)
         )
         .arg(
+            Arg::new("encrypt")
+                .short('e')
+                .long("encrypt")
+                .help("Encrypts the input file"),
+        )
+        .arg(
+            Arg::new("decrypt")
+                .short('d')
+                .long("decrypt")
+                .help("Decrypts the input file"),
+        )
+        .arg(
             Arg::with_name("key")
                 .short('k')
                 .long("key")
                 .value_name("FILE")
                 .help("Sets the encryption key (dictionary) file")
                 .takes_value(true)
-        ).getMatches();
+        ).get_matches();
 
     let is_encrypt = matches.is_present("encrypt");
     let is_decrypt = matches.is_present("decrypt");
@@ -46,15 +68,6 @@ fn main() -> io::Result<()> {
     };
 
     fs::write(output_file, processed_text).expect("Error writing to output file");
-}
 
-fn caesar_cipher(input: &str, key: i32) -> String {
-    input.to_uppercase()
-        .chars()
-        .filter(|c| c.is_alphabetic())
-        .map(|c| {
-            let first_letter = b'A';
-            let shifted = c as u8 - first_letter + key.rem_euclid(26) as u8 + first_letter % 26;
-            shifted as char
-    }).collect::<String>().to_uppercase()
+    Ok(())
 }
