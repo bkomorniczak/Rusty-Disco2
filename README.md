ed70b720a0c45217b83a17e078cf0a6ee26204d3
komob <b.komorniczak@gmail.com>
2024-04-13 13:51:04 +0200

    reformat

--- README.md
+++ README.md
@@ -3,5 +3,8 @@
 `cargo run -- -i src/resource/encrypted.txt -o src/resource/decrypted.txt -k 3 -d --caesar   
 `
 `cargo run -- -i src/resource/plain.txt -o src/resource/encrypted.txt -k 3 -e --caesar `
+
 `cargo run -- -i src/resource/plain.txt -o src/resource/encrypted.txt -e --affine -a 2 -b 5 `
+
+`cargo run -- -i src/resource/encrypted.txt -o src/resource/decrypted.txt -d --affine -a 2 -b 5 `
 
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod affine;
 
 use clap::{App, Arg};
 use std::{fs, io};
+use crate::affine::brute_force_affine;
 use crate::brute_force::brute_force_caesar;
 
 
@@ -11,87 +12,95 @@ fn main() -> io::Result<()> {
     let matches = App::new("Cezar")
         .version("1.0")
         .about("Lab2 KK")
-        .arg(
-            Arg::with_name("input")
+        .arg(Arg::with_name("input")
                 .short('i')
                 .long("input")
                 .help("Sets the input plaintext file")
                 .takes_value(true)
         )
-        .arg(
-            Arg::with_name("output")
+        .arg(Arg::with_name("output")
                 .short('o')
                 .long("output")
                 .help("Sets the output encrypted file")
                 .takes_value(true)
         )
-        .arg(
-            Arg::new("encrypt")
+        .arg(Arg::new("encrypt")
                 .short('e')
                 .long("encrypt")
                 .help("Encrypts the input file"),
         )
-        .arg(
-            Arg::new("decrypt")
+        .arg(Arg::new("decrypt")
                 .short('d')
                 .long("decrypt")
                 .help("Decrypts the input file"),
         )
-        .arg(
-            Arg::with_name("key")
+        .arg(Arg::with_name("key")
                 .short('k')
                 .long("key")
-                .value_name("FILE")
                 .help("Sets the encryption key (dictionary) file")
                 .takes_value(true)
         )
-        .arg(
-            Arg::with_name("caesar")
+        .arg(Arg::with_name("caesar")
                 .long("caesar")
                 .help("Sets cipher mode to caesar")
         )
-        .arg(
-            Arg::with_name("affine")
+        .arg(Arg::with_name("affine")
                 .long("affine")
                 .help("Sets cipher mode to affine")
         )
-        .arg(
-            Arg::with_name("brute_force")
+        .arg(Arg::with_name("brute_force")
                 .long("brute")
                 .help("Runs bruteforce attack on input")
         )
-        .arg(
-            Arg::with_name("a")
+        .arg(Arg::with_name("a")
                 .short('a')
-                .help("Sets variable to affine cipher")
+                .help("Sets variable a for affine cipher")
                 .takes_value(true)
         )
-        .arg(
-            Arg::with_name("b")
+        .arg(Arg::with_name("b")
                 .short('b')
-                .help("Sets variable to affine cipher")
+                .help("Sets variable b for affine cipher")
                 .takes_value(true)
         )
         .get_matches();
+
+    let input_file = matches.value_of("input").expect("Input file is required");
+    let input_text = fs::read_to_string(input_file).expect("Error reading input file");
+
 
     let is_encrypt = matches.is_present("encrypt");
     let is_decrypt = matches.is_present("decrypt");
     let is_caesar = matches.is_present("caesar");
     let is_affine = matches.is_present("affine");
     let is_brute_force = matches.is_present("brute_force");
-    let mut a = 0;
-    let mut b = 0;
-    if is_affine {
-        a = matches.value_of("a").unwrap().parse::<i32>().expect("variable must be an integer");
-        b = matches.value_of("b").unwrap().parse::<i32>().expect("variable must be an integer");
-    }
-    let mut key: i32 = 0;
-    if matches.is_present("key") { key = matches.value_of("key").unwrap().parse::<i32>().expect("Key must be an integer"); }
-    let input_file = matches.value_of("input").unwrap();
-    let mut output_file = "";
-    if matches.is_present("output") { output_file = matches.value_of("output").unwrap(); }
 
-    let input_text = fs::read_to_string(input_file).expect("Error reading input file");
+
+    let (a,b) = if is_affine {
+        let a = matches.value_of("a")
+            .expect("Variable a is requiered for affine cypher")
+            .parse::<i32>()
+            .expect("Variable must be an integer");
+        let b = matches.value_of("b")
+            .expect("Variable b is required for affine cypher")
+            .parse::<i32>()
+            .expect("variable must be an integer");
+        (a,b)
+    } else {
+        (0,0)
+    };
+
+    let  key =  if matches.is_present("key") {
+        matches.value_of("key")
+            .expect("Key is neccesary")
+            .parse::<i32>()
+            .expect("Key must be an integer")
+    } else {
+        0
+    };
+
+
+
+
     let mut processed_text = "".parse::<String>().unwrap();
      if !is_brute_force {
          processed_text = if is_encrypt {
@@ -115,9 +124,17 @@ fn main() -> io::Result<()> {
         panic!("Either encrypt or decrypt must be specified");
     }};
 
-    if matches.is_present("output") { fs::write(output_file, processed_text.clone()).expect("Error writing to output file"); }
-    if is_brute_force {
+    if let Some(output_file) = matches.value_of("output") {
+        fs::write(output_file, &processed_text)
+            .expect("Failed to write to output file");
+    }
+
+    if is_brute_force && is_caesar {
         println!("{:?}", brute_force_caesar(&input_text));
+    }
+
+    if is_brute_force && is_affine {
+        println!("{:?}", brute_force_affine(&input_text))
     }
     Ok(())
 }
