1b129be9a05fd703f7920884b1293fa9afa65a44
komob <b.komorniczak@gmail.com>
2024-04-13 09:51:47 +0200

    removed non-alphabetic chars from affine

--- src/affine.rs
+++ src/affine.rs
@@ -3,8 +3,8 @@ fn inverted_mod(a: i32) -> Option<i32> {
     let mut xy = (0, 1);
 
     while mn.1 != 0 {
-        xy = (xy.1, xy.0 - (mn.0 /mn.1) * xy.1);
-        mn = (mn.1, mn.0% mn.1);
+        xy = (xy.1, xy.0 - (mn.0 / mn.1) * xy.1);
+        mn = (mn.1, mn.0 % mn.1);
     }
 
     if mn.0 > 1 { return None; }
@@ -12,24 +12,24 @@ fn inverted_mod(a: i32) -> Option<i32> {
     Some(xy.0)
 }
 
-pub fn encrypt (text: &str, a: i32, b:i32) -> String {
+pub fn encrypt(text: &str, a: i32, b: i32) -> String {
     text.to_uppercase().chars()
         .filter(|c| c.is_alphabetic())
         .map(|c| {
-            let x = c as i32 - 'a' as i32;
-            let y = (a*x +b) % 26;
-            (y + 'a' as i32) as u8 as char
-    })
+            let x = c as i32 - 'A' as i32;
+            let y = ((a * x + b) % 26 + 26) % 26;
+            (y + 'A' as i32) as u8 as char
+        })
         .collect()
 }
 
 pub fn decrypt(text: &str, a: i32, b: i32) -> Option<String> {
-    inverted_mod(a).map(|a_inv|{
+    inverted_mod(a).map(|a_inv| {
         text.chars()
             .map(|c| {
                 if c.is_alphabetic() {
                     let x = c as i32 - 'a' as i32;
-                    let y = ((x -b) * a_inv % 26) % 26;
+                    let y = ((x - b) * a_inv % 26) % 26;
                     (y + 'a' as i32) as u8 as char
                 } else {
                     c
